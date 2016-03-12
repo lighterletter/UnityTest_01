5d5d6e5fd6bdfc6c0bc717b20882631c8f96110e
John Gomez <jgomez818@gmail.com>
2016-03-12 11:40:34 -0500

    initial commit

---
+++ Assets/movecube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movecube : MonoBehaviour {
+
+	public float direction;
+	public float horizontal;
+	public float vertical;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontal = Input.GetAxis ("Horizontal");
+		vertical = Input.GetAxis ("Vertical");
+
+		//transform.position += direction;
+	}
+}
